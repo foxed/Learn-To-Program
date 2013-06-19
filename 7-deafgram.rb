16838a4263cb7d5b8f52791941b2d735db391d71
Alex Fox <alexdysonfox@gmail.com>
2013-06-19 17:55:20 -0400

    Create 7-deafgram.rb
    Deaf Grandma exercises at the end of chapter 7.
    I struggled with these for quite a bit..

---
+++ 7-deafgram.rb
@@ -0,0 +1,17 @@
+#First Deaf Grandma Exercise
+
+puts ''
+
+while true
+input = gets.chomp 
+  if input != input.upcase
+    puts 'SPEAK UP!!'
+   else input == input.upcase
+year = 1920 + rand(18)
+      puts 'OH NO, NOT SINCE, ' + year.to_s + '!' 
+        if input == 'BYE' 
+           break
+        end
+  end    
+end
+
