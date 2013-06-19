e35ea7a728bde33aa5e11bc0db9db8509b1f3299
Alex Fox <alexdysonfox@gmail.com>
2013-06-19 18:19:29 -0400

    Create 3-exercises.rb

---
+++ 3-exercises.rb
@@ -0,0 +1,32 @@
+#Chapter 3 Exercises 
+
+puts 1+2
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+puts 5 * (12-8) + -15
+puts 98 + (59872 / (13*8)) * -51
+puts (24 * 365)
+puts (60 * 24) * (365 * 10)
+puts (60 * 24) * (365 * 22)
+
+puts ''
+
+puts 'Hello, world!' 
+puts ' '
+puts 'Goodbye, world!'
+
+puts ''
+
+puts 'I like ' + 'apple pie.'
+puts 'blink ' * 4
+
+puts ''
+
+puts  12  +  12  #Integers
+puts '12' + '12' #Strings
+puts '12  +  12'
+
+puts ''
+
+puts 'You\'re swell!'
