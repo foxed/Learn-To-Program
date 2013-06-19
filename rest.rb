49ce7b12d114133a442178cefa043c879bd277f2
Alex Fox <alexdysonfox@gmail.com>
2013-06-19 17:27:19 -0300

    Create rest.rb

---
+++ rest.rb
@@ -0,0 +1,12 @@
+puts 'Hello there, and what\'s your name?'
+name = gets.chomp
+puts 'Your name is ' + name + '? What a lovely name!'
+puts 'Pleased to meet you, ' + name + ' . :)' 
+puts ''
+puts 'Hello there, what\'s your first name?'
+fname = gets.chomp
+puts 'Hello, ' + fname + ', what is your middle name?'
+mname = gets.chomp
+puts 'Ah, ' + mname + ', what an interesting middle name! How about your last name?'
+lname = gets.chomp
+puts 'Nice to meet you, ' + fname + '' + mname + '' + lname + '!'
