0710378f907765518db41bb81e654af32ae2f76a
Alex Fox <alexdysonfox@gmail.com>
2013-06-19 18:58:42 -0300

    Update 7-deafgram.rb
    these exercises were much easier after I got a better grasp of how looping/while/if conditions worked

--- 7-deafgram.rb
+++ 7-deafgram.rb
@@ -15,3 +15,19 @@ year = 1920 + rand(18)
   end    
 end
 
+#Second Deaf Grandma Exercise
+
+puts ''
+
+while true
+input = gets.chomp 
+ if input != input.upcase
+    puts 'SPEAK UP!!'
+   else input == input.upcase
+year = 1920 + rand(18)
+      puts 'OH NO, NOT SINCE, ' + year.to_s + '!' 
+        if input == 'BYE ' * 3 
+           break
+        end
+   end    
+end
