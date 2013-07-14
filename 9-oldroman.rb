0892c7f7016cd0b1258af0bff6572d98326c8c7f
Alex Fox <alexdysonfox@gmail.com>
2013-07-14 17:54:50 -0400

    Update 9-oldroman.rb

--- 9-oldroman.rb
+++ 9-oldroman.rb
@@ -5,7 +5,7 @@ def old_roman_num num
 roman = '' 
 roman = roman + 'M' * (num       / 1000) 
 roman = roman + 'D' * (num % 1000/  500) 
-roman = roman + 'C' * (num %  500/  100)#C represents 100; this is C multiplied by the number of times 100 goes into 5000 (i.e., its remainder). So if the remainder is 3, the figure CCC should appear. (i.e., romanized 1300 would be MCCC)
+roman = roman + 'C' * (num %  500/  100)#C represents 100; this is C multiplied by the number of times 100 goes into 500 (i.e., its remainder). So if the remainder is 3, the figure CCC should appear. (i.e., romanized 1300 would be MCCC)
 roman = roman + 'L' * (num %  100/   50)
 roman = roman + 'X' * (num %   50/   10)
 roman = roman + 'V' * (num %   10/    5)
