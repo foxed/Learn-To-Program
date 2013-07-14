be6752e6d9eb5dfbfdb3ac9677b43d7602c78adc
Alex Fox <alexdysonfox@gmail.com>
2013-07-14 19:27:55 -0400

    10-englishnumber.rb

---
+++ 10-englishnumber.rb
@@ -0,0 +1,88 @@
+
+def english_number number 
+	if number < 0
+		return 'Please insert a number greater than 0' 
+	end
+	if number == 0
+		return 'zero'
+	end
+
+	num_string = ''
+	ones_place = [ 'one',  'two','three',
+			 'four', 'five', 'six', 
+		    'seven','eight','nine']
+    tens_place = ['ten', 'twenty', 'thirty',
+    			 'fourty', 'fifty', 'sixty',
+    			 'seventy', 'eighty', 'ninety']
+	teens_place = ['eleven', 'twelve','thirteen',
+	          'fourteen', 'fifteen', 'sixteen',
+	          'seventeen', 'eighteen','nineteen']
+
+left = number 
+write = left/1000000 
+left = left - write*1000000
+
+if write > 0
+	millions = english_number write
+	num_string = num_string + millions + ' million'
+  if left > 0
+	num_string = num_string + '-'
+  end
+end
+
+write = left/1000
+left = left - write*1000
+
+if write > 0
+	thousands = english_number write
+	num_string = num_string + thousands + ' thousand'
+  if left > 0
+	num_string = num_string + '-'
+  end
+end
+
+
+write = left/100
+left = left - write*100
+
+if write > 0
+    	hundreds = english_number write
+		num_string = num_string + hundreds + ' hundred'
+   if left > 0
+	 num_string = num_string + '-'
+   end
+end
+
+write = left/10
+left = left - write*10
+
+	if write > 0
+		if ((write == 1) and (left > 0))
+			num_string = num_string + teens_place[left-1]
+			left = 0
+		else
+			num_string = num_string + tens_place[write-1]
+		end
+	if left > 0
+		num_string = num_string + '-'
+	end
+end
+
+write = left
+left = 0
+
+if write > 0
+	num_string = num_string + ones_place[write-1]
+end
+
+num_string
+
+end
+
+puts english_number ( 0)
+puts english_number (14)
+puts english_number (500)
+puts english_number (1356)
+puts english_number (1450000)
+puts english_number (78330)
+puts english_number (5000532)
