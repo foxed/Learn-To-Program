a7cd019efd0531fa596a92b320b35f26f58a62a8
Alex Fox <alexdysonfox@gmail.com>
2013-07-14 14:29:00 -0400

    Create 9-oldroman.rb
    I had a lot of trouble with this exercise and had to look at the answer to fully comprehend how to do it. Math is not my forte. I do understand the concepts behind the math, however.

---
+++ 9-oldroman.rb
@@ -0,0 +1,19 @@
+#Convert numeric numbers to old roman numerals 
+
+
+def old_roman_num num
+roman = '' 
+roman = roman + 'M' * (num       / 1000) 
+roman = roman + 'D' * (num % 1000/  500) 
+roman = roman + 'C' * (num %  500/  100)#C represents 100; this is C multiplied by the number of times 100 goes into 5000 (i.e., its remainder). So if the remainder is 3, the figure CCC should appear. (i.e., romanized 1300 would be MCCC)
+roman = roman + 'L' * (num %  100/   50)
+roman = roman + 'X' * (num %   50/   10)
+roman = roman + 'V' * (num %   10/    5)
+roman = roman + 'I' * (num %    5/    1)
+roman
+ 
+end
+puts(old_roman_num(1990))
+puts(old_roman_num(405))
+puts(old_roman_num(400))
+puts(old_roman_num(1300))
